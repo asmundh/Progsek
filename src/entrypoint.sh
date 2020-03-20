65a77c1f727eb278a7b419c551bd6800b95b8f58
Mikkel Benjamin Lerberg Nygard <mbnygard@stud.ntnu.no>
2020-03-20 22:22:39 +0100

    Enable HTTPS

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -10,12 +10,29 @@ USE_STATIC_PATH=${STATIC_PATH:-'/app/static'}
 # Get the listen port for Nginx, default to 8080
 USE_LISTEN_PORT=${LISTEN_PORT:-8080}
 
-#if [ -f /app/nginx.conf ]; then
+# if [ -f /app/nginx.conf ]; then
 #    cat /app/nginx.conf > /etc/nginx/conf.d/nginx.conf
-#else
+# else
+# content_server='server {\n'
+# content_server=$content_server"    listen ${USE_LISTEN_PORT};\n"
+# content_server=$content_server'    location / {\n'
+# content_server=$content_server'        include uwsgi_params;\n'
+# content_server=$content_server'        uwsgi_pass unix:///tmp/uwsgi.sock;\n'
+# content_server=$content_server'    }\n'
+# content_server=$content_server"    location $USE_STATIC_URL {\n"
+# content_server=$content_server"        alias $USE_STATIC_PATH;\n"
+# content_server=$content_server'    }\n'
+# content_server=$content_server'}\n'
 
-content_server='server {\n'
-content_server=$content_server"    listen ${USE_LISTEN_PORT};\n"
+
+# Configuration skeleton for using SSL
+# https://nginx.org/en/docs/http/configuring_https_servers.html
+content_server=$content_server'server {\n'
+content_server=$content_server"    listen 443 ssl http2;\n"
+content_server=$content_server' ssl_certificate \"/app/server.crt\";\n' 
+content_server=$content_server' ssl_certificate_key \"/app/server.key";\n'
+#content_server=$content_server'    ssl_protocols       <optional protocols>;\n'
+#content_server=$content_server'    ssl_ciphers         <optional ciphers>;\n'
 content_server=$content_server'    location / {\n'
 content_server=$content_server'        include uwsgi_params;\n'
 content_server=$content_server'        uwsgi_pass unix:///tmp/uwsgi.sock;\n'
@@ -27,24 +44,6 @@ content_server=$content_server'}\n'
 
 content_server=$content_server"add_header X-Frame-Options sameorigin always;"
 content_server=$content_server"add_header X-Content-Type-Options nosniff always;"
-
-
-# Configuration skeleton for using SSL
-# https://nginx.org/en/docs/http/configuring_https_servers.html
-#content_server=$content_server'server {\n'
-#content_server=$content_server"    listen 443 ssl http2;\n"
-#content_server=$content_server'    ssl_certificate      <certificate .crt file>;\n'
-#content_server=$content_server'    ssl_certificate_key     <certificate .key file>;\n'
-#content_server=$content_server'    ssl_protocols       <optional protocols>;\n'
-#content_server=$content_server'    ssl_ciphers         <optional ciphers>;\n'
-#content_server=$content_server'    location / {\n'
-#content_server=$content_server'        include uwsgi_params;\n'
-#content_server=$content_server'        uwsgi_pass unix:///tmp/uwsgi.sock;\n'
-#content_server=$content_server'    }\n'
-#content_server=$content_server"    location $USE_STATIC_URL {\n"
-#content_server=$content_server"        alias $USE_STATIC_PATH;\n"
-#content_server=$content_server'    }\n'
-#content_server=$content_server'}\n'
 
 
 # Save generated server /etc/nginx/conf.d/nginx.conf
